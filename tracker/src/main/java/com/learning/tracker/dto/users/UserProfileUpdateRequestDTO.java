2ce2d481b87857566accf8bfdcddabba77d891bc
Rag-gy <kraghulprasanth@gmail.com>
2025-07-25 19:49:55 +0530

    Updating dto name

--- tracker/src/main/java/com/learning/tracker/dto/users/UserProfileUpdateRequestDTO.java
+++ tracker/src/main/java/com/learning/tracker/dto/users/UserProfileUpdateRequestDTO.java
@@ -3,7 +3,7 @@ package com.learning.tracker.dto.users;
 import com.learning.tracker.enums.UserRoleEnum;
 import jakarta.validation.constraints.Size;
 
-public record userProfileUpdateRequestDTO(
+public record UserProfileUpdateRequestDTO(
     @Size(min = 2, max = 50, message = "First name must be between 2 and 50 characters long")
     String firstName,
     @Size(min = 2, max = 50, message = "Last name must be between 2 and 50 characters long")
